2ad3bbc29f8c7dad30e4b7fc47006c56a60a993a
Robel tewolde <rmtewo81@harrisonburg.k12.va.us>
2019-09-17 13:33:21 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,13 +2,16 @@ var score = 0;
 
 postToPage(score); // Print to the page
 
+createButton('Robel', btnPress); // Make a button
 createButton('Button', btnPress); // Make a button
 changeTitle('Clicker Game');
 
 setInterval(btnPress, 1000);
 
 function btnPress() {
-  score++;
+  score--;
   postToPage(score);
-}
 
+  function robel() {
+    score++;
+}
