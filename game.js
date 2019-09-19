78a256b8f7fb933f9c9e1dbbfdc96d777bd1cc58
Robel tewolde <rmtewo81@harrisonburg.k12.va.us>
2019-09-19 14:32:30 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
-var x = new Button("Robel", btn)
+var score = 0; // Create a variable to store the score in
 
-function btn() {
-  x.edit(randomID())
+var title = new Title('Welcome to the Game'); // Create a title element
+var scr = new Text(score);                     // Create a text element
 
+setInterval(btnPress, 1000);   // Set up a loop
+
+function btnPress() {
+  score--;         // Increase the score
+  scr.edit(score); // Update the page with the new score
 }
