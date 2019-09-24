a9e439645132773590bd7db27ec51d3cdab31b6d
Robel tewolde <rmtewo81@harrisonburg.k12.va.us>
2019-09-24 14:11:28 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,39 @@
-var score = 0; // Create a variable to store the score in
+//var score = 0; // Create a variable to store the score in
 
-var title = new Title('Welcome to the Game'); // Create a title element
-var scr = new Text(score);                     // Create a text element
+//var title = new Title('Welcome to the Game'); // Create a title element
+//var scr = new Text(score);                     // Create a text element
 
-setInterval(btnPress, 1000);   // Set up a loop
+//setInterval(btnPress, 1000);   // Set up a loop
+
+//function btnPress() {
+//  score--;         // Increase the score
+//  scr.edit(score); // Update the page with the new score
+
+var score = 1; // Create a variable to store the score in
+
+var title = new Title('Story'); // Create a title element
+var button = new Button('hello', btnPress);   // Create a button element
+var hidden = new Button('Boo!');
+hidden.hide();
+var scr = new Text(score);                    // Create a text element
+
+createButton('Robel', btnPress); // Make a button
+createButton('Button', btnPress); // Make a button
+changeTitle('Clicker Game');
 
 function btnPress() {
-  score--;         // Increase the score
+  score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
+  if(score > 10) {
+    hidden.show();
+  }
+  if(score > 15) {
+    hidden.hide(500);
+  }
+}
+
+function increment() {
+  score++;
+}
+
 }
