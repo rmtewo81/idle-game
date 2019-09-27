ec1b83dffd14cf86e2760cc769402e256aea958a
Robel tewolde <rmtewo81@harrisonburg.k12.va.us>
2019-09-27 07:57:09 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,17 +1,19 @@
 var score = 1; // Create a variable to store the score in
 
+var materials = new Button('Materials', button ); // Make a button
+var restart = new Button('Restart' ); // Make a button
+var build = new Button('Build' ); // Make a button
+var build = new Button('Collect brick' ); // Make a button
+
 var title = new Title('The Ether'); // Create a title element
 var button = new Button('Start', );   // Create a button element
 var hidden = new Button('Boo!');
 hidden.hide();
 var scr = new Text("Welcome.");                    // Create a text element
-var scr = new Text("Here you can be whoever you want to be."); 
+var scr = new Text("Here you can be whoever you want to be.");
 var scr = new Text("Now begin your journey.");
 
-createButton('Materials' ); // Make a button
-createButton('Restart' ); // Make a button
-createButton('Build' ); // Make a button
-changeTitle('Clicker Game');
+
 
 function btnPress() {
   if(score > 10) {
@@ -24,4 +26,8 @@ function btnPress() {
 
 function increment() {
   score++;
+}
+
+function button() {
+  var scr = new Text('Collect brick')
 }
