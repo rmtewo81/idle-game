e58aa108afcafb93205fcf6c639b236116e26dfd
Robel tewolde <rmtewo81@harrisonburg.k12.va.us>
2019-09-27 08:17:25 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,16 +1,16 @@
 var score = 1; // Create a variable to store the score in
 
-var materials = new Button('Materials', button ); // Make a button
+var materials = new Button('Materials' ); // Make a button
 var restart = new Button('Restart' ); // Make a button
 var build = new Button('Build' ); // Make a button
 var build = new Button('Collect brick' ); // Make a button
 
 var title = new Title('The Ether'); // Create a title element
-var button = new Button('Start', );   // Create a button element
+var button = new Button('Start', button );   // Create a button element
 var hidden = new Button('Boo!');
 hidden.hide();
 var scr = new Text("Welcome.");                    // Create a text element
-var scr = new Text("Here you can be whoever you want to be.");
+var scr = new Text("You will try to survive and progress.");
 var scr = new Text("Now begin your journey.");
 
 
