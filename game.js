0ef0bba8aeb6ec0b336965030791e840de4f7543
Robel tewolde <rmtewo81@harrisonburg.k12.va.us>
2019-09-27 14:22:43 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 var score = 1; // Create a variable to store the score in
 
-var materials = new Button('Materials' ); // Make a button
+var materials = new Button('Materials', mat ); // Make a button
 var restart = new Button('Restart' ); // Make a button
 var build = new Button('Build' ); // Make a button
-var build = new Button('Collect brick' ); // Make a button
+var build = new Button('Collect brick', brick ); // Make a button
 
 var title = new Title('The Ether'); // Create a title element
 var button = new Button('Start', button );   // Create a button element
@@ -31,3 +31,24 @@ function increment() {
 function button() {
   var scr = new Text('Collect brick')
 }
+function mat() {
+  var list = new HTMLElement('ul'); // make a container for the list
+  var listItem1 = new HTMLElement('li', 'leather:0'); // Make a list item
+  var listItem2 = new HTMLElement('li', 'metal:0');
+  var listItem3 = new HTMLElement('li', 'meat:0');
+  var listItem4 = new HTMLElement('li', 'animals:0');
+  var listItem5 = new HTMLElement('li', 'guards:0');
+  list.add(listItem1); // add the list item to the container
+  list.add(listItem2)
+  list.add(listItem3)
+  list.add(listItem4)
+  list.add(listItem5)
+}
+function brick() {
+  var collect = new Text('you have collected 10 brick')
+}
+function build() {
+  var build = new HTMLElement('ul')
+  var buildItem1 = new HTMLElement('li', 'house:0')
+  var buildItem2 = new HTMLElement('li', 'Fence:0')
+  var buildItem1 = new HTMLElement('li', 'house:0')
